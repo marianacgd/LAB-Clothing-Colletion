e3d2749c7533a93d0e255dcfce8846fb6e01cdea
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-21 19:18:45 -0300

    componente box modal

---
+++ LAB-Clothing-Collection/src/app/components/box-login/box-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box-login',
+  templateUrl: './box-login.component.html',
+  styleUrls: ['./box-login.component.scss']
+})
+export class BoxLoginComponent {
+
+}
