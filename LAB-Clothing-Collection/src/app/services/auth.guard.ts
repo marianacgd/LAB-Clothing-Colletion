f5bcffa857ee72cceb6d6cec791ee8564293821c
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-24 23:17:04 -0300

    guard

---
+++ LAB-Clothing-Collection/src/app/services/auth.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UsuarioService } from './usuario.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const userLogado = sessionStorage.getItem('logado') || 'false';
+    if (userLogado === "true") {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+  
+}
