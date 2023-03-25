30cd4f4125f7d300757708308153fe7c4637b919
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-25 19:18:40 -0300

    Componente Card Dashboard

---
+++ LAB-Clothing-Collection/src/app/components/card-dashboard/card-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-dashboard',
+  templateUrl: './card-dashboard.component.html',
+  styleUrls: ['./card-dashboard.component.scss']
+})
+export class CardDashboardComponent {
+
+}
