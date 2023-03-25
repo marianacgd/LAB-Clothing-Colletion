36bb67de303aaffc3650fd1c1a6b5db0c22720c9
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-25 19:56:24 -0300

    Tela Coleções

---
+++ LAB-Clothing-Collection/src/app/pages/colecoes/colecoes.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ColecoesComponent } from './colecoes.component';
+
+describe('ColecoesComponent', () => {
+  let component: ColecoesComponent;
+  let fixture: ComponentFixture<ColecoesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ColecoesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ColecoesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
