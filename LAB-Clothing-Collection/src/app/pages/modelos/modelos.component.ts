e7042638d9d5000dc1fd1b4edfbc8b70a579aabc
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-26 20:39:38 -0300

    Tela Modelos

--- LAB-Clothing-Collection/src/app/pages/modelos/modelos.component.ts
+++ LAB-Clothing-Collection/src/app/pages/modelos/modelos.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IModelo } from 'src/app/interfaces/modelo';
+import { ModeloService } from 'src/app/services/modelo.service';
 
 @Component({
   selector: 'app-modelos',
   templateUrl: './modelos.component.html',
   styleUrls: ['./modelos.component.scss']
 })
-export class ModelosComponent {
+export class ModelosComponent implements OnInit {
+
+  modelos: IModelo[] = [];
+
+  constructor (private modeloService: ModeloService) {}
+
+  ngOnInit(): void {
+
+    this.listarModelos()
+    
+  }
+
+  listarModelos(){
+    this.modeloService.getModelos().subscribe((data) => {
+      this.modelos = data;
+    })
+  }
 
 }
