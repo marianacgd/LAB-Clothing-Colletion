8fe1a8d4173ff3f388557efec9be66ab5f60c804
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-26 18:57:07 -0300

    Serviço Modelo

---
+++ LAB-Clothing-Collection/src/app/services/modelo.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IModelo } from "../interfaces/modelo";
+import { IColecao } from "../interfaces/colecao";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModeloService {
+
+  private baseUrl = 'http://localhost:3000/modelos';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getModelos(): Observable<IModelo[]> {
+    return this.http.get<IModelo[]>(this.baseUrl)
+  }
+
+  getModelosPorId(id: number): Observable<IModelo> {
+    return this.http.get<IModelo>( `${this.baseUrl}/id=${id}`)
+  }
+
+  getModelosPorIdColecao(idColecao: number): Observable<IModelo> {
+    return this.http.get<IModelo>( `${this.baseUrl}/id=${idColecao}`)
+  }
+  
+  postModelo(modelo: IModelo): Observable<IModelo> {
+    return this.http.post<IModelo>(this.baseUrl, modelo)
+  }
+
+  putModelo(modelo: IModelo): Observable<IModelo> {
+    return this.http.put<IModelo>(this.baseUrl, modelo)
+  }
+
+  deleteModelo(id: number){
+    return this.http.delete<IModelo>(`${this.baseUrl}/id=${id}`)
+  }
+}
