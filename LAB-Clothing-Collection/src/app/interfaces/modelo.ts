3a36412b949a8beeb3107c537960465f5b9afe5b
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-26 18:54:28 -0300

    Interface Modelo

---
+++ LAB-Clothing-Collection/src/app/interfaces/modelo.ts
@@ -0,0 +1,10 @@
+
+export interface IModelo {
+    id: number;
+    nome: string;
+    tipo: string;
+    idColecao: number;
+    responsavel: string;
+    possuiEstampa: boolean;
+    possuiBordado: boolean;
+}
