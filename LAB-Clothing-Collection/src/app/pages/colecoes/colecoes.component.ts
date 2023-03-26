d21eceb7ad59139d2be9eaaf13c13398a4686c31
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-26 20:39:15 -0300

    Tela Coleções

--- LAB-Clothing-Collection/src/app/pages/colecoes/colecoes.component.ts
+++ LAB-Clothing-Collection/src/app/pages/colecoes/colecoes.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IColecao } from 'src/app/interfaces/colecao';
+import { ColecaoService } from 'src/app/services/colecao.service';
 
 @Component({
   selector: 'app-colecoes',
   templateUrl: './colecoes.component.html',
   styleUrls: ['./colecoes.component.scss']
 })
-export class ColecoesComponent {
+export class ColecoesComponent implements OnInit {
+
+  colecoes: IColecao[] = [];
+
+  constructor(private colecaoService: ColecaoService){}
+
+  ngOnInit(): void {
+
+    this.listarColecoes()
+    
+  }
+
+  listarColecoes(){
+    this.colecaoService.getColecoes().subscribe((data) => {
+      this.colecoes = data;
+    })
+  }
 
 }
