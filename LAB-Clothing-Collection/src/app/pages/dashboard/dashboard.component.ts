71cc009dde6eaaee5dd36bac409ca14b20dda0dc
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-26 20:39:29 -0300

    Tela Dashboard

--- LAB-Clothing-Collection/src/app/pages/dashboard/dashboard.component.ts
+++ LAB-Clothing-Collection/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IColecao } from 'src/app/interfaces/colecao';
+import { ColecaoService } from 'src/app/services/colecao.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  colecoes: IColecao[] = [];
+
+  constructor(private colecaoService: ColecaoService){}
+
+  ngOnInit(): void {
+
+    this.listarColecoesMaiorOrcamento();
+    
+  }
+
+  listarColecoesMaiorOrcamento(){
+    this.colecaoService.getColecoesMaxValorDesc().subscribe((data) => {
+      this.colecoes = data;
+    })
+  }
 
 }
