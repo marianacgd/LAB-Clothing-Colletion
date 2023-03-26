32fea50757c5d549dd01844eaa96ecdcbcd87642
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-26 18:56:53 -0300

    Serviço Coleção

---
+++ LAB-Clothing-Collection/src/app/services/colecao.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IColecao } from "../interfaces/colecao";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColecaoService {
+
+  private baseUrl = 'http://localhost:3000/colecoes';
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getColecoes(): Observable<IColecao[]> {
+    return this.http.get<IColecao[]>(this.baseUrl)
+  }
+
+  getColecaoPorId(id: number): Observable<IColecao> {
+    return this.http.get<IColecao>( `${this.baseUrl}/id=${id}`)
+  }
+  
+  getColecoesMaxValorDesc(){
+    return this.http.get<IColecao[]>( `${this.baseUrl}?_sort=orcamento&_order=desc&_limit=5`)
+  }
+
+  postColecao(colecao: IColecao): Observable<IColecao> {
+    return this.http.post<IColecao>(this.baseUrl, colecao)
+  }
+
+  putColecao(colecao: IColecao): Observable<IColecao> {
+    return this.http.put<IColecao>(this.baseUrl, colecao)
+  }
+
+  deleteColecao(id: number): Observable<IColecao> {
+    return this.http.delete<IColecao>( `${this.baseUrl}/id=${id}`)
+  }
+}
