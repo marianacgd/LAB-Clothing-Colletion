ad5cad08acc5e9ae9941dc61fa619f0b2662e46c
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-28 02:33:45 -0300

    Tela Dashboard

--- LAB-Clothing-Collection/src/app/pages/dashboard/dashboard.component.ts
+++ LAB-Clothing-Collection/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IColecao } from 'src/app/interfaces/colecao';
+import { IModelo } from 'src/app/interfaces/modelo';
 import { ColecaoService } from 'src/app/services/colecao.service';
+import { ModeloService } from 'src/app/services/modelo.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,20 +11,48 @@ import { ColecaoService } from 'src/app/services/colecao.service';
 })
 export class DashboardComponent implements OnInit {
 
+  colecoesMaior: IColecao[] = [];
   colecoes: IColecao[] = [];
+  numeroDeColecoes: string = '';
+  modelos: IModelo[] = [];
+  numeroDeModelos: string = '';
 
-  constructor(private colecaoService: ColecaoService){}
+  constructor(private colecaoService: ColecaoService, private modeloService: ModeloService){}
 
   ngOnInit(): void {
 
     this.listarColecoesMaiorOrcamento();
-    
+    this.listarColecoes();
+    this.listarModelos();    
   }
 
   listarColecoesMaiorOrcamento(){
-    this.colecaoService.getColecoesMaxValorDesc().subscribe((data) => {
-      this.colecoes = data;
+    this.colecaoService.getColecoesMaxValorTop5().subscribe((data) => {
+      this.colecoesMaior = data;
+      console.log(this.colecoesMaior);
     })
+    
+  }
+
+  listarColecoes(){
+    this.colecaoService.getColecoes().subscribe((data) => {
+      this.colecoes = data;
+      console.log(this.colecoes.length);
+    })
+    this.numeroDeColecoes = this.colecoes.length.toString();
+  }
+
+  listarModelos(){
+    this.modeloService.getModelos().subscribe((data) => {
+      this.modelos = data;
+      console.log(this.modelos.length);
+    })
+    this.numeroDeModelos = this.modelos.length.toString();
+  }
+
+  orcamentoMedio(){
+      
   }
 
 }
+
