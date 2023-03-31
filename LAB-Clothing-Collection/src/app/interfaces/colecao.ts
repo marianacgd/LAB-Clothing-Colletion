94d522f20e22b8263337b2f4f1048fb8a0e4e8fe
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-31 16:21:32 -0300

    Interface Coleção

--- LAB-Clothing-Collection/src/app/interfaces/colecao.ts
+++ LAB-Clothing-Collection/src/app/interfaces/colecao.ts
@@ -2,12 +2,12 @@ import { IModelo } from "./modelo";
 
 
 export interface IColecao {
-    id: number;
+    id: string;
     nomeColecao: string;
     responsavel: string;
     estacao: string;
     ano: number;
-    modelos: IModelo[];
+    modelo: IModelo;
     orcamento: number;
     marca: string;
 }
