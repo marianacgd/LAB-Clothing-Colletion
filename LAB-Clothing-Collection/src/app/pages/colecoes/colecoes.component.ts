98efffe217b0401539e8aa331fa051c2d6cdde12
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-31 16:25:33 -0300

    Tela Listagem de Coleções

--- LAB-Clothing-Collection/src/app/pages/colecoes/colecoes.component.ts
+++ LAB-Clothing-Collection/src/app/pages/colecoes/colecoes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IColecao } from 'src/app/interfaces/colecao';
+import { IModelo } from 'src/app/interfaces/modelo';
 import { ColecaoService } from 'src/app/services/colecao.service';
+import { ModeloService } from 'src/app/services/modelo.service';
 
 @Component({
   selector: 'app-colecoes',
@@ -10,19 +13,33 @@ import { ColecaoService } from 'src/app/services/colecao.service';
 export class ColecoesComponent implements OnInit {
 
   colecoes: IColecao[] = [];
+  modelosPorColecao: string = '';
 
-  constructor(private colecaoService: ColecaoService){}
+
+  constructor(private colecaoService: ColecaoService, private router: Router, private modeloService: ModeloService){}
 
   ngOnInit(): void {
 
     this.listarColecoes()
-    
+        
   }
 
   listarColecoes(){
     this.colecaoService.getColecoes().subscribe((data) => {
       this.colecoes = data;
+      this.colecoes.filter((value) => this.listarModelosPorColecao(value))
     })
+  }
+
+  listarModelosPorColecao(colecao: IColecao) {
+    this.modeloService.getModelosPorIdColecao(Number(colecao.id)).subscribe((data) => {
+      console.log(data);
+      colecao.modelo = data;
+    });
+  }
+
+  editarColecao(colecao: IColecao){
+    this.router.navigate([`/criarcolecao/${colecao.id}`])
   }
 
 }
