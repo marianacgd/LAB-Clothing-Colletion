110cb446b83bd26475a6082082cce117b5ded776
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-31 16:26:38 -0300

    Componente Menu

--- LAB-Clothing-Collection/src/app/components/menu/menu.component.ts
+++ LAB-Clothing-Collection/src/app/components/menu/menu.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }  
 
 }
