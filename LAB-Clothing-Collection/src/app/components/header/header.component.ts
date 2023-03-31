9595490994eb83d3b37cc282755b5a69565d166e
Mariana Daruix <mariana.daruix@gmail.com>
2023-03-31 16:26:51 -0300

    Componente Header

--- LAB-Clothing-Collection/src/app/components/header/header.component.ts
+++ LAB-Clothing-Collection/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Placement as PopperPlacement, Options } from '@popperjs/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  logout(){}
 }
