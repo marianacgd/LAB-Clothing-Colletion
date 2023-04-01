b590144d5128c04eeb629b38f3d84171c2eda8fa
Mariana Daruix <mariana.daruix@gmail.com>
2023-04-01 17:39:11 -0300

    Tela Esqueci a Senha

--- LAB-Clothing-Collection/src/app/pages/esqueci-senha/esqueci-senha.component.ts
+++ LAB-Clothing-Collection/src/app/pages/esqueci-senha/esqueci-senha.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-esqueci-senha',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./esqueci-senha.component.scss']
 })
 export class EsqueciSenhaComponent {
+  email: string = "";
+  formAlterarSenha!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.formAlterarSenha = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  alterarSenha(){
+    if(!this.formAlterarSenha.valid){
+      alert('E-mail inválido!');
+      return;
+    }
+
+    const email = this.formAlterarSenha.controls['email'].value;
+    this.email = email;
+  }
 
 }
